4a831fc4c9554117cec38fbfb97f8e20df4aad3e
PrawiraGenestonlia <prawira.96@gmail.com>
2019-09-15 00:26:04 +0800

    .

---
+++ screens/authloading.screen.js
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+import {
+  ActivityIndicator,
+  AsyncStorage,
+  StatusBar,
+  StyleSheet,
+} from 'react-native';
+import { Button, Layout, Text } from 'react-native-ui-kitten';
+
+export default function AuthLoadingScreen(props) {
+  const { navigation } = props;
+  // Fetch the token from storage then navigate to our appropriate place
+  useEffect(() => {
+    _bootstrapAsync();
+  }, [])
+
+  _bootstrapAsync = async () => {
+    try {
+      const userToken = '';
+      // userToken = await AsyncStorage.getItem('userToken');
+      AsyncStorage.getItem('userToken', (err, result) => {
+        // console.log(result);
+        navigation.navigate(result ? 'Main' : 'Auth');
+      });
+      // navigation.navigate(userToken ? 'Main' : 'Auth');
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <Layout style={styles.container}>
+      <ActivityIndicator />
+      <StatusBar barStyle="default" />
+    </Layout>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text: {
+    marginVertical: 16,
+  },
+});
--- screens/index.js
+++ screens/index.js
@@ -1,8 +1,10 @@
 import LoginScreen from './login.screen';
 import SettingScreen from './settings.screen';
 import MyxBin from './myxbin.screen';
+import AuthLoadingScreen from './authloading.screen';
 
 export {
+  AuthLoadingScreen,
   LoginScreen,
   SettingScreen,
   MyxBin
